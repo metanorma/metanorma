f272ee15004f13f8bf6212b3dfb9dccd7ea6ed98
Nick Nicholas <opoudjis@optusnet.com.au>
2018-09-25 21:26:22 +1000

    rspec

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -1,8 +1,9 @@
 require_relative "spec_helper"
+require "fileutils"
 
 RSpec.describe Metanorma::Output::Pdf do
   it "generates a PDF" do
-    system "rm -f a.html a.pdf"
+    FileUtils.rm_f %w(a.html a.pdf)
     File.open("a.html", "w") { |f| f.write("<html><head/><body><h1>Hello</h1></body></html>") }
     generator = Metanorma::Output::Pdf.new
     generator.convert("a.html", "a.pdf")
