faac1468eaff6c1c2e34c872f54a82e98e26f6ec
Nick Nicholas <opoudjis@optusnet.com.au>
2018-11-29 22:42:13 +1100

    phantomjs

--- lib/metanorma/output/pdf.rb
+++ lib/metanorma/output/pdf.rb
@@ -1,14 +1,17 @@
+require "phantomjs"
+
 module Metanorma
   module Output
     class Pdf < Base
 
       def convert(url_path, output_path)
         file_url = "file://#{Dir.pwd}/#{url_path}"
-        pdfjs = File.join(File.dirname(__FILE__), '../../../bin/metanorma-pdf.js')
-        system "export NODE_PATH=$(npm root --quiet -g);
-                      node #{pdfjs} #{file_url} #{output_path}"
+        #pdfjs = File.join(File.dirname(__FILE__), '../../../bin/metanorma-pdf.js')
+        #system "export NODE_PATH=$(npm root --quiet -g); node #{pdfjs} #{file_url} #{output_path}"
+        Phantomjs.path
+        pdfjs = File.join(File.dirname(__FILE__), '../../../bin/rasterize.js')
+        Phantomjs.run(pdfjs, file_url, output_path)
       end
-
     end
   end
 end
