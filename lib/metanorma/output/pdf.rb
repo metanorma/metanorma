c1cf6a64fcfacfea850762275ab5389c80ec3eee
Nick Nicholas <opoudjis@optusnet.com.au>
2019-01-18 15:44:05 +0000

    convert PDFs in directories outside working directory; https://github.com/riboseinc/metanorma/issues/37

--- lib/metanorma/output/pdf.rb
+++ lib/metanorma/output/pdf.rb
@@ -3,7 +3,9 @@ module Metanorma
     class Pdf < Base
 
       def convert(url_path, output_path)
-        file_url = "file://#{Dir.pwd}/#{url_path}"
+        file_url = url_path
+        file_url = "file://#{url_path}" if %r{^/}.match file_url
+        file_url = "file://#{Dir.pwd}/#{url_path}" unless %r{^file://}.match file_url
         pdfjs = File.join(File.dirname(__FILE__), '../../../bin/metanorma-pdf.js')
         ENV['NODE_PATH'] ||= `npm root --quiet -g`.strip
         system "node #{pdfjs} #{file_url} #{output_path}"
