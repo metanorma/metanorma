da2ca36ff97225c5ff5653e091622e00727d7b52
Nick Nicholas <opoudjis@gmail.com>
2019-09-26 19:02:51 +1000

    rspec

--- spec/compile_spec.rb
+++ spec/compile_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Metanorma::Compile do
     expect(File.exist?("spec/assets/test1.html")).to be true
     expect(File.exist?("spec/assets/test1.alt.html")).to be false
     expect(File.exist?("spec/assets/test1.relaton.xml")).to be true
-    xml = File.read("spec/assets/test1.xml")
+    xml = File.read("spec/assets/test1.xml", encoding: "utf-8")
     expect(xml).to include "</iso-standard>"
   end
 
@@ -39,14 +39,14 @@ RSpec.describe Metanorma::Compile do
     expect(File.exist?("spec/assets/test.doc")).to be true
     expect(File.exist?("spec/assets/test.html")).to be false
     expect(File.exist?("spec/assets/test.alt.html")).to be false
-    xml = File.read("spec/assets/test.xml")
+    xml = File.read("spec/assets/test.xml", encoding: "utf-8")
     expect(xml).to include "</iso-standard>"
   end
 
   it "extracts isodoc options from asciidoc file" do
     FileUtils.rm_f %w(spec/assets/test.xml spec/assets/test.html spec/assets/test.alt.html spec/assets/test.doc)
     Metanorma::Compile.new().compile("spec/assets/test.adoc", { type: "iso", extension_keys: [:html] } )
-    html = File.read("spec/assets/test.html")
+    html = File.read("spec/assets/test.html", encoding: "utf-8")
     expect(html).to include "font-family: body-font;"
     expect(html).to include "font-family: header-font;"
     expect(html).to include "font-family: monospace-font;"
