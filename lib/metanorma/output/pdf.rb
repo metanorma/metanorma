78976dccf3a008db8393d262406abec8e80b2ec4
Alexande B <abobrikovich@gmail.com>
2020-03-28 22:54:48 +0300

    #100 Correctly handle paths with spaces

--- lib/metanorma/output/pdf.rb
+++ lib/metanorma/output/pdf.rb
@@ -1,5 +1,6 @@
 require "open3"
 require "pathname"
+require "shellwords"
 
 module Metanorma
   module Output
@@ -12,10 +13,19 @@ module Metanorma
         pdfjs = File.join(File.dirname(__FILE__), "../../../bin/metanorma-pdf.js")
 
         node_path = ENV["NODE_PATH"] || `npm root --quiet -g`.strip
-        node_cmd = ["node", pdfjs, file_url, output_path].join(" ")
+        node_cmd = ["node", pdfjs, file_url, output_path].map { |arg| shellescape(arg) }.join(" ")
 
         _, error_str, status = Open3.capture3({ "NODE_PATH" => node_path }, node_cmd)
         raise error_str unless status.success?
+      end
+
+      def shellescape(str)
+        if Gem.win_platform?()
+          # https://bugs.ruby-lang.org/issues/16741
+          str.match(" ") ? "\"#{str}\"" : str
+        else
+          Shellwords.shellescape(str)
+        end
       end
     end
   end
--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -2,32 +2,61 @@ require_relative "spec_helper"
 require "fileutils"
 
 RSpec.describe Metanorma::Output::Pdf do
+  ASSETS_DIR = 'spec/assets/test'
+  INPUT = "#{ASSETS_DIR}/a.html"
+  INPUT_CONTENT = "<html><head/><body><h1>Hello</h1></body></html>"
+  OUTPUT = "#{ASSETS_DIR}/a.pdf"
+  DROP = [INPUT, OUTPUT]
+
   it "generates a PDF" do
-    FileUtils.rm_f %w(a.html a.pdf)
-    File.write("a.html", "<html><head/><body><h1>Hello</h1></body></html>")
+    FileUtils.rm_f DROP
+    FileUtils.mkdir_p ASSETS_DIR
+    File.write(INPUT, INPUT_CONTENT)
+
     generator = Metanorma::Output::Pdf.new
-    generator.convert("a.html", "a.pdf")
-    expect(File.exist?("a.pdf")).to be true
+
+    generator.convert(INPUT, OUTPUT)
+    expect(File.exist?(OUTPUT)).to be true
   end
 
   it "handle absolute path" do
-    FileUtils.rm_f %w(a.html a.pdf)
-    File.write("a.html", "<html><head/><body><h1>Hello</h1></body></html>")
+    FileUtils.rm_f DROP
+    FileUtils.mkdir_p ASSETS_DIR
+    File.write(INPUT, INPUT_CONTENT)
+
     generator = Metanorma::Output::Pdf.new
-    generator.convert(File.join(Dir.pwd, "a.html"), File.join(Dir.pwd, "a.pdf"))
-    expect(File.exist?(File.join(Dir.pwd, "a.pdf"))).to be true
+
+    generator.convert(File.join(Dir.pwd, INPUT), File.join(Dir.pwd, OUTPUT))
+    expect(File.exist?(File.join(Dir.pwd, OUTPUT))).to be true
   end
 
   it "handle url as input" do
-    FileUtils.rm_f %w(a.html a.pdf)
-    File.write("a.html", "<html><head/><body><h1>Hello</h1></body></html>")
+    FileUtils.rm_f DROP
+    FileUtils.mkdir_p ASSETS_DIR
+    File.write(INPUT, INPUT_CONTENT)
+
     generator = Metanorma::Output::Pdf.new
-    generator.convert("file://#{File.join(Dir.pwd, "a.html")}", "a.pdf")
-    expect(File.exist?("a.pdf")).to be true
+
+    generator.convert("file://#{File.join(Dir.pwd, INPUT)}", OUTPUT)
+    expect(File.exist?(OUTPUT)).to be true
   end
 
   it "Missing input path" do
     generator = Metanorma::Output::Pdf.new
     expect { generator.convert("random.html", "random.pdf") }.to raise_error
+  end
+
+  it "Path with spaces" do
+    dir = File.join(ASSETS_DIR, "dir with path")
+    input = File.join(dir, "a.html")
+    output = File.join(dir, "a.pdf")
+    FileUtils.rm_f [input, output]
+    FileUtils.mkdir_p dir
+    File.write(input, INPUT_CONTENT)
+    generator = Metanorma::Output::Pdf.new
+
+    generator.convert(input, output)
+
+    expect(File.exist?(output)).to be true
   end
 end
