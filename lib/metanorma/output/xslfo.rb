a17034aad929e7aed6d4e2b4d3ee7e4d1d7a1da8
Nick Nicholas <opoudjis@gmail.com>
2020-04-30 20:30:02 +1000

    jar update: closes https://github.com/metanorma/metanorma-iso/issues/361

--- lib/metanorma/output/xslfo.rb
+++ lib/metanorma/output/xslfo.rb
@@ -8,8 +8,9 @@ module Metanorma
       def convert(url_path, output_path, xsl_stylesheet)
         return if url_path.nil? || output_path.nil? || xsl_stylesheet.nil?
         pdfjar = File.join(File.dirname(__FILE__), "../../../bin/mn2pdf.jar")
-        cmd = ["java", "-jar", pdfjar, "--xml-file", url_path, "--xsl-file",
-               xsl_stylesheet, "--pdf-file", output_path].join(" ")
+        cmd = ["java", "-Xss5m", "-Xmx1024m", "-jar", pdfjar, "--xml-file", 
+               url_path, "--xsl-file", xsl_stylesheet, "--pdf-file",
+               output_path].join(" ")
         _, error_str, status = Open3.capture3(cmd)
         raise error_str unless status.success?
       end
