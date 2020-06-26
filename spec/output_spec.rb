53b8291878f53723f8f3fadf29fc5070984ba343
Nick Nicholas <opoudjis@gmail.com>
2020-06-26 23:29:29 +1000

    rspec

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -1,21 +1,23 @@
 require_relative "spec_helper"
 require "fileutils"
 
-RSpec.describe Metanorma::Output::Pdf do
+RSpec.describe Metanorma::Output::XslfoPdf do
   ASSETS_DIR = 'spec/assets/test'
-  INPUT = "#{ASSETS_DIR}/a.html"
+  INPUT = "#{ASSETS_DIR}/a.xml"
   INPUT_CONTENT = "<html><head/><body><h1>Hello</h1></body></html>"
   OUTPUT = "#{ASSETS_DIR}/a.pdf"
   DROP = [INPUT, OUTPUT]
+  XSL = "#{ASSETS_DIR}/iso.international-standard.xsl"
 
   it "generates a PDF" do
     FileUtils.rm_f DROP
     FileUtils.mkdir_p ASSETS_DIR
     File.write(INPUT, INPUT_CONTENT)
 
-    generator = Metanorma::Output::Pdf.new
+    generator = Metanorma::Output::XslfoPdf.new
 
-    generator.convert(INPUT, OUTPUT)
+    generator.convert("spec/assets/test/a.xml", "a.pdf", XSL)
+    #generator.convert(INPUT, OUTPUT, XSL)
     expect(File.exist?(OUTPUT)).to be true
   end
 
@@ -24,9 +26,9 @@ RSpec.describe Metanorma::Output::Pdf do
     FileUtils.mkdir_p ASSETS_DIR
     File.write(INPUT, INPUT_CONTENT)
 
-    generator = Metanorma::Output::Pdf.new
+    generator = Metanorma::Output::XslfoPdf.new
 
-    generator.convert(File.join(Dir.pwd, INPUT), File.join(Dir.pwd, OUTPUT))
+    generator.convert(File.join(Dir.pwd, INPUT), File.join(Dir.pwd, OUTPUT), XSL)
     expect(File.exist?(File.join(Dir.pwd, OUTPUT))).to be true
   end
 
@@ -35,9 +37,9 @@ RSpec.describe Metanorma::Output::Pdf do
     FileUtils.mkdir_p ASSETS_DIR
     File.write(INPUT, INPUT_CONTENT)
 
-    generator = Metanorma::Output::Pdf.new
+    generator = Metanorma::Output::XslfoPdf.new
 
-    generator.convert("file://#{File.join(Dir.pwd, INPUT)}", OUTPUT)
+    generator.convert("file://#{File.join(Dir.pwd, INPUT)}", OUTPUT, XSL)
     expect(File.exist?(OUTPUT)).to be true
   end
 
@@ -48,25 +50,17 @@ RSpec.describe Metanorma::Output::Pdf do
 
   it "Path with spaces" do
     dir = File.join(ASSETS_DIR, "dir with path")
-    input = File.join(dir, "a.html")
+    input = File.join(dir, "a.xml")
     output = File.join(dir, "a.pdf")
     FileUtils.rm_f [input, output]
     FileUtils.mkdir_p dir
     File.write(input, INPUT_CONTENT)
-    generator = Metanorma::Output::Pdf.new
+    generator = Metanorma::Output::XslfoPdf.new
 
     generator.convert(input, output)
+    generator.convert(input, output, XSL)
 
     expect(File.exist?(output)).to be true
-  end
-end
-
-RSpec.describe Metanorma::Output::XslfoPdf do
-  it "generates a PDF from HTML" do
-    FileUtils.rm_f %w(a.pdf)
-    generator = Metanorma::Output::XslfoPdf.new
-    generator.convert("spec/assets/a.xml", "a.pdf", File.join(File.dirname(__FILE__), "assets", "iso.international-standard.xsl"))
-    expect(File.exist?("a.pdf")).to be true
   end
 end
 
