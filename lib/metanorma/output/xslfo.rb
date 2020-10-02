8ec9ec2819da5903c436d9035e264f2f18e94b99
Nick Nicholas <opoudjis@gmail.com>
2020-10-03 02:08:45 +1000

    allow mn2pdf options: https://github.com/metanorma/mn2pdf/issues/45

--- lib/metanorma/output/xslfo.rb
+++ lib/metanorma/output/xslfo.rb
@@ -4,10 +4,10 @@ require_relative "./utils.rb"
 module Metanorma
   module Output
     class XslfoPdf < Base
-      def convert(url_path, output_path, xsl_stylesheet)
+      def convert(url_path, output_path, xsl_stylesheet, options = "")
         return if url_path.nil? || output_path.nil? || xsl_stylesheet.nil?
 
-        Mn2pdf.convert(quote(url_path), quote(output_path), quote(xsl_stylesheet))
+        Mn2pdf.convert(quote(url_path), quote(output_path), quote(xsl_stylesheet), options)
       end
 
       def quote(x)
