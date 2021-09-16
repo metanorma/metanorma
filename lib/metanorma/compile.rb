54c9e1668327d9950427c4e15c2f487090791528
Nick Nicholas <opoudjis@gmail.com>
2021-09-17 00:49:08 +1000

    add base-asset-path parameter: https://github.com/metanorma/metanorma-cli/issues/242

--- lib/metanorma/compile.rb
+++ lib/metanorma/compile.rb
@@ -244,9 +244,8 @@ module Metanorma
     def get_isodoc_options(file, options, ext)
       isodoc_options = @processor.extract_options(file)
       isodoc_options[:datauriimage] = true if options[:datauriimage]
-      isodoc_options[:baseassetpath] = true if options[:baseassetpath]
       isodoc_options[:sourcefilename] = options[:filename]
-      %i(bare sectionsplit no_install_fonts).each do |x|
+      %i(bare sectionsplit no_install_fonts baseassetpath).each do |x|
         isodoc_options[x] ||= options[x]
       end
       if ext == :pdf
