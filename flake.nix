23ea2d7fff44080e3fe9754dc22567a5fe8895b8
Jeffrey Lau <jeffrey.lau@ribose.com>
2025-02-25 14:37:10 +0800

    Add Nix flake support for local development
    ```console
    $ cd metanorma
    $ nix develop
    ...
    $ # Now you are in a shell with all dependencies installed.
    ```
    
    And if direnv and nix-direnv are installed:
    
    ```console
    $ cd metanorma
    ...
    $
    $ # Now you are in a shell with all dependencies installed.
    ```
    
    direnv: https://direnv.net/
    nix-direnv: https://github.com/nix-community/nix-direnv
    
    Related to (but does not fix) #62

---
+++ flake.nix
@@ -0,0 +1,118 @@
+{
+  description = "Ruby Dev Env";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell/main";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , devshell
+    , flake-compat
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      cwd = builtins.toString ./.;
+      overlays = map (x: x.overlays.default) [
+        devshell
+      ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      runtimeDependencies = with pkgs; [
+        graphviz
+        inkscape
+        jre
+        plantuml
+        xml2rfc
+      ];
+    in
+    rec {
+
+      # nix develop
+      devShell = pkgs.devshell.mkShell {
+
+        env = [
+        ];
+        commands = [
+          {
+            name = "metanorma";
+            command = "exe/metanorma \"$@\"";
+            help = "Run Metanorma CLI";
+            category = "App";
+          }
+          {
+            name = "release";
+            command = "bundle exec rake release \"$@\"";
+            help = "Run rake release, which adds a tag and pushes to RubyGems";
+            category = "Ruby";
+          }
+          {
+            name = "lint";
+            command = "bundle exec rubocop \"$@\"";
+            help = "Run rubocop";
+            category = "Ruby";
+          }
+          {
+            name = "update-flakes";
+            command = "make update-flakes \"$@\"";
+            help = "Update all flakes";
+            category = "Nix";
+          }
+        ] ++
+        # Only append these if there is no .tool-verions file
+        # to avoid conflicts:
+        (if ! builtins.pathExists ./.tool-versions then
+          [{
+
+            name = "irb";
+            command = "bundle exec irb \"$@\"";
+            help = "Run console IRB (has completion menu)";
+            category = "Ruby";
+          }
+            {
+              name = "console";
+              command = "bundle exec irb \"$@\"";
+              help = "Run console IRB (has completion menu)";
+              category = "Ruby";
+            }
+            {
+              name = "pry";
+              command = "bundle exec pry \"$@\"";
+              help = "Run pry";
+              category = "Ruby";
+            }
+            {
+              name = "rspec";
+              command = "bundle exec rspec \"$@\"";
+              help = "Run test suite";
+              category = "Ruby";
+            }]
+        else [ ]);
+
+        packages = with pkgs; [
+          # rubocop                  # Install with your Gemfile / gemspec
+          # ruby                     # Install with your favourite Ruby version manager, etc.
+          # rubyPackages.solargraph  # Bring your own
+          # rubyfmt                  # Broken
+          bash
+          curl
+          fd
+          fzf
+          gnused
+          jq
+          ripgrep
+          rubyPackages.ruby-lsp
+          rubyPackages.sorbet-runtime
+          wget
+        ] ++
+        runtimeDependencies;
+
+      };
+    });
+}
