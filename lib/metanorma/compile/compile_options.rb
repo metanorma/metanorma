7675331fa001edab9aea2ea3094a1a6878889866
Nick Nicholas <opoudjis@gmail.com>
2025-06-02 21:52:58 +1000

    debug i18nyaml option

--- lib/metanorma/compile/compile_options.rb
+++ lib/metanorma/compile/compile_options.rb
@@ -92,7 +92,7 @@ module Metanorma
 
       def get_isodoc_options(file, options, ext)
         ret = @processor.extract_options(file)
-        dir = options[:filename].sub(%r(/[^/]+$), "/")
+        dir = File.dirname(options[:filename])
         ret[:i18nyaml] &&= File.join(dir, ret[:i18nyaml])
         copy_isodoc_options_attrs(options, ret)
         font_manifest_mn2pdf(options, ret, ext)
